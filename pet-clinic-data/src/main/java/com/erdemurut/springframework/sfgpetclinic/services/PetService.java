6831459336818b670719fc74205687f92487f2f6
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-04-03 14:15:28 +0300

    Refactored to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/services/PetService.java
@@ -2,12 +2,5 @@ package com.erdemurut.springframework.sfgpetclinic.services;
 
 import com.erdemurut.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-	Pet findById(Long id);
-
-	Pet save(Pet pet);
-
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
