631d3fea135f40b3032cd36fa53e494764e4ff4f
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-05-26 16:42:02 +0300

    Added PetTypeService. Closes #28

---
+++ pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.erdemurut.springframework.sfgpetclinic.services;
+
+import com.erdemurut.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
