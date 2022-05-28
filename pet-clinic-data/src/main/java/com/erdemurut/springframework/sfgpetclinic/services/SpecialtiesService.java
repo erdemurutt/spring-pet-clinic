07ac319c2fbb508561fbbfdf046c045726637d34
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-05-28 17:33:15 +0300

    Added Specialties Map Implementation. Closes #30

---
+++ pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.erdemurut.springframework.sfgpetclinic.services;
+
+import com.erdemurut.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
