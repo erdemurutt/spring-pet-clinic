8942db5832892a7b8aae53ad9d87c2785de90d59
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-06-01 19:38:05 +0300

    Added Spring data JPA service for Pet. Closes #42

--- pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,6 +4,5 @@ import com.erdemurut.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
 	Owner findByLastName(String lastName);
 }
