84f3e250face10e65192283e5b5867269496d6c7
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-06-01 20:06:44 +0300

    Implemented Map Impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.erdemurut.springframework.sfgpetclinic.services;
+
+import com.erdemurut.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
