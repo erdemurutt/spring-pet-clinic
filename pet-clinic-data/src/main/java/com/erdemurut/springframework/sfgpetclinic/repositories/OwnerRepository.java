173aa620fb0817d4799819f20b1bec2ac76011d7
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-07-02 16:21:15 +0300

    Find Owner implemented. Closes #54

--- pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,10 @@ package com.erdemurut.springframework.sfgpetclinic.repositories;
 import com.erdemurut.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 	Owner findByLastName(String lastName);
+
+	List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/erdemurut/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.erdemurut.springframework.sfgpetclinic.services;
 
 import com.erdemurut.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
 	Owner findByLastName(String lastName);
+
+	List<Owner> findAllByLastNameLike(String lastName);
 }
