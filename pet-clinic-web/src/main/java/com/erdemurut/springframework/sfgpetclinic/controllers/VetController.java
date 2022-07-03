0484ffbc80bcb35386424852352f12ee2962930f
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-07-03 21:55:11 +0300

    Added json endpoint for vets. Closes #66

--- pet-clinic-web/src/main/java/com/erdemurut/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/erdemurut/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.erdemurut.springframework.sfgpetclinic.controllers;
 
+import com.erdemurut.springframework.sfgpetclinic.model.Vet;
 import com.erdemurut.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -15,9 +20,13 @@ public class VetController {
 
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
 	public String listVets(Model model) {
-
 		model.addAttribute("vets", vetService.findAll());
-
 		return "vets/index";
+	}
+
+	@GetMapping("/api/vets")
+	@ResponseBody
+	public Set<Vet> getVetsJson() {
+		return vetService.findAll();
 	}
 }
